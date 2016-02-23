fc453b575a1e3f323ae85a14ee9647602fdf5858
Jon Coulter <jonrcoulter@gmail.com>
2016-02-23 16:23:45 -0500

    delete

--- cache/compiled/files/a5c615b9ad01df435f596b807b10107d.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://ceevee/ceevee.yaml',
-    'modified' => 1455638723,
-    'data' => [
-        'enabled' => true
-    ]
-];
